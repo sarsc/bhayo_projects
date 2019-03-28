15cf9e0a9c79f26be68f579ce2353f382a71e38a
Sara Scaini <sa.scaini@gmail.com>
2019-03-28 14:12:02 +0000

    implemented #index, #show, and #new in projects c

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,15 +1,23 @@
 class ProjectsController < ApplicationControlle
+  before_action :set_project, only: [:show, :create]
 
   def index
+    @projects = Project.all
   end
 
   def show
   end
 
   def new
+    @project = Project.new
   end
 
   def create
   end
 
+  private
+
+  def set_projects
+    @project = Project.find(:id)
+  end
 end
