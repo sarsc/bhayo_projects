10519d6ff9e92b59888406d257f9abd55958dc67
Sara Scaini <sa.scaini@gmail.com>
2019-03-29 09:56:55 +0000

    created images controller and modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :projects do
-    resources :images, only: [:create, :edit, :update]
+    resources :images, only: [:new, :create, :edit, :update]
   end
 end
