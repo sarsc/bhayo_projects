9671bcb39bba3a8003df88e1b0afcf87bb92ccbb
Sara Scaini <sa.scaini@gmail.com>
2019-03-29 11:39:28 +0000

    display multiple images in project show

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,7 +3,21 @@ class ImagesController < ApplicationController
     @project = Project.find(params[:project_id])
     @image = Image.new
   end
+
   def create
-    # @project = Project.find(parmams[project_id])
+    @project = Project.find(params[:project_id])
+    @image = Image.new(image_params)
+    @image.project = @project
+    if @image.save
+      redirect_to project_path(@project)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:photo_url)
   end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,5 @@ class Image < ApplicationRecord
   validates :photo_url, presence: true
   belongs_to :project
 
-  mount_uploader :photo, PhotoUploader
+  mount_uploader :photo_url, PhotoUploader
 end
