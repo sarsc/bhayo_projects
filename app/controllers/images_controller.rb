3f67adc4ce7c8a22bb10348ca8b813ca0d8071a2
Sara Scaini <sa.scaini@gmail.com>
2019-03-29 10:16:48 +0000

    implemented #new in images c and created form

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,3 +1,9 @@
 class ImagesController < ApplicationController
-
+  def new
+    @project = Project.find(params[:project_id])
+    @image = Image.new
+  end
+  def create
+    # @project = Project.find(parmams[project_id])
+  end
 end
