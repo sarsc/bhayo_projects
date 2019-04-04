a2db374c5fc7d82f189ef3a6c8aa8c3d5990abce
Sara Scaini <sa.scaini@gmail.com>
2019-04-04 13:24:03 +0100

    added column to projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
+  validates :category, presence: true
   has_many :images, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_03_105229) do
+ActiveRecord::Schema.define(version: 2019_04_04_121810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_04_03_105229) do
     t.string "client"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
