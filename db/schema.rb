4ea999f860226640a5e6258e247e4d52cadde020
Sara Scaini <sa.scaini@gmail.com>
2019-04-29 16:49:54 +0100

    added description to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_04_121810) do
+ActiveRecord::Schema.define(version: 2019_04_29_154727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_04_04_121810) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
