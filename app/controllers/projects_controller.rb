98ca2f49c49890197eb4f3c8c78dc7c06d105eba
Sara Scaini <sa.scaini@gmail.com>
2019-05-04 18:33:37 +0100

    added destroy action in project controller and delete link in the index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
-  before_action :set_project, only: [:show, :edit, :update]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
     if params[:category].present?
@@ -32,6 +32,11 @@ class ProjectsController < ApplicationController
   def update
     @project.update(project_params)
     redirect_to project_path
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
   end
 
   private
