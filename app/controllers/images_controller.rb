20012debf6160e8a71c59a30b19b698559841a6b
Sara Scaini <sa.scaini@gmail.com>
2019-05-04 19:15:42 +0100

    addes destroy action to image controller and delete link for image in project show

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,13 +1,12 @@
 class ImagesController < ApplicationController
-  before_action :set_image, only: [:edit, :update]
+  before_action :set_image, only: [:edit, :update, :destroy]
+  before_action :project_id, only: [:new, :create]
 
   def new
-    @project = Project.find(params[:project_id])
     @image = Image.new
   end
 
   def create
-    @project = Project.find(params[:project_id])
     @image = Image.new(image_params)
     @image.project = @project
     if @image.save
@@ -25,6 +24,11 @@ class ImagesController < ApplicationController
     redirect_to project_path(@image.project)
   end
 
+  def destroy
+    @image.destroy
+    redirect_to project_path(@image.project)
+  end
+
   private
 
   def image_params
@@ -33,5 +37,9 @@ class ImagesController < ApplicationController
 
   def set_image
     @image = Image.find(params[:id])
+  end
+
+  def project_id
+    @project = Project.find(params[:project_id])
   end
 end
