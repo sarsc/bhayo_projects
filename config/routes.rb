31908b882cb2f50569c5fee8ac4a38917d42ce8f
Sara Scaini <sa.scaini@gmail.com>
2019-05-09 21:55:45 +0100

    added about and contact routes end created relative views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations"}
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/contact'
+  get 'pages/about'
 
   resources :projects do
     resources :images, only: [:new, :create]
