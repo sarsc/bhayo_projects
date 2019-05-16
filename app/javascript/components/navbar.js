f3da82ba7f4bf036a0b05dbedc97f62d637809be
Sara Scaini <sa.scaini@gmail.com>
2019-05-16 19:03:09 +0100

    show logo homepage on scroll navbar

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,10 +1,15 @@
 const updateNavbarOnScroll = () => {
 const navbar = document.querySelector('.navbar-b')
+const logo = document.getElementById('logo')
 window.addEventListener('scroll', ()=> {
   if (window.scrollY >= window.innerHeight) {
     navbar.classList.add('navbar-color');
+    logo.classList.remove('d-none');
+
   } else {
     navbar.classList.remove('navbar-color');
+    logo.classList.add('d-none');
+
   }
 })
 };
